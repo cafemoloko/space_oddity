8a34e0c24fe1c10fe7f5c2bc97b9ce4936235284
cafemoloko <karolina.pawlaczyk@gmail.com>
2025-01-27 19:03:35 +0100

    first commit

---
+++ space_oddity_counter.py
@@ -0,0 +1,20 @@
+# How many times has David Bowie's 'Spece Oddity' been played since Tesla Roadset launched into space?
+# Inspired by: https://www.astronomy.com/science/astronomers-just-deleted-an-asteroid-because-it-turned-out-to-be-elon-musks-tesla-roadster/
+
+from datetime import datetime
+
+# Tesla Roadster launch date: February 6, 2018
+launch_date = datetime(2018, 2, 6)
+current_date = datetime.today()
+
+time_elapsed = current_date - launch_date
+total_seconds_elapsed = time_elapsed.total_seconds()
+
+# Song duration in seconds (5 minutes 15 seconds)
+song_duration_seconds = 5 * 60 + 15
+
+times_played = total_seconds_elapsed // song_duration_seconds
+
+formatted_times_played = "{:,.0f}".format(times_played).replace(",", " ")
+print(formatted_times_played)
+
